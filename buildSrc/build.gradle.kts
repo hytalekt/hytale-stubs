bcabde68bff81bb5375172465372fed88e995ede
oglassdev <23040613+oglassdev@users.noreply.github.com>
2025-12-17 23:30:27 -0500

    chore: setup project

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    `kotlin-dsl`
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.plugin.kotlin)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.bundles.asm)
+}
