78cae6ffefdaa85598dcbf7e6c157e319ddd6719
oglassdev <23040613+oglassdev@users.noreply.github.com>
2026-01-20 01:27:53 -0500

    chore: switch deps from compileOnly to api

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,35 +3,38 @@ import io.github.hytalekt.stubs.task.ProcessStubsTask
 import org.gradle.kotlin.dsl.register
 
 plugins {
-    java
-    idea
+    `java-library`
+    `maven-publish`
 }
+
+group = "io.github.hytalekt.stubs"
+version = "0.1.0-alpha.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compileOnly("com.google.code.findbugs:jsr305:3.0.2")
-    compileOnly("com.google.code.gson:gson:2.11.0")
-    compileOnly("org.mongodb:bson:5.3.1")
-    compileOnly("io.netty:netty-all:4.2.9.Final")
-    compileOnly("io.netty.incubator:netty-incubator-codec-classes-quic:0.0.74.Final")
-    compileOnly("com.google.guava:guava:33.4.0-jre")
-    compileOnly("org.slf4j:slf4j-api:2.0.16")
-    compileOnly("it.unimi.dsi:fastutil:8.5.15")
-    compileOnly("org.joml:joml:1.10.8")
-    compileOnly("com.google.flogger:flogger:0.8")
-    compileOnly("com.google.flogger:flogger-system-backend:0.8")
-    compileOnly("io.sentry:sentry:8.29.0")
-    compileOnly("org.checkerframework:checker-qual:3.48.3")
-    compileOnly("net.sf.jopt-simple:jopt-simple:5.0.4")
-    compileOnly("org.jline:jline:3.26.3")
-    compileOnly("com.nimbusds:nimbus-jose-jwt:9.41.1")
-    compileOnly("ch.randelshofer:fastdoubleparser:1.0.0")
-    compileOnly("com.github.luben:zstd-jni:1.5.6-4")
-    compileOnly("org.bouncycastle:bcprov-jdk18on:1.78.1")
-    compileOnly("org.bouncycastle:bcutil-jdk18on:1.78.1")
+    api("com.google.code.findbugs:jsr305:3.0.2")
+    api("com.google.code.gson:gson:2.11.0")
+    api("org.mongodb:bson:5.3.1")
+    api("io.netty:netty-all:4.2.9.Final")
+    api("io.netty.incubator:netty-incubator-codec-classes-quic:0.0.74.Final")
+    api("com.google.guava:guava:33.4.0-jre")
+    api("org.slf4j:slf4j-api:2.0.16")
+    api("it.unimi.dsi:fastutil:8.5.15")
+    api("org.joml:joml:1.10.8")
+    api("com.google.flogger:flogger:0.8")
+    api("com.google.flogger:flogger-system-backend:0.8")
+    api("io.sentry:sentry:8.29.0")
+    api("org.checkerframework:checker-qual:3.48.3")
+    api("net.sf.jopt-simple:jopt-simple:5.0.4")
+    api("org.jline:jline:3.26.3")
+    api("com.nimbusds:nimbus-jose-jwt:9.41.1")
+    api("ch.randelshofer:fastdoubleparser:1.0.0")
+    api("com.github.luben:zstd-jni:1.5.6-4")
+    api("org.bouncycastle:bcprov-jdk18on:1.78.1")
+    api("org.bouncycastle:bcutil-jdk18on:1.78.1")
 }
 
 sourceSets {
@@ -55,9 +58,38 @@ tasks.register<ProcessStubsTask>("processStubs") {
 }
 
 tasks.compileJava {
-    dependsOn("processStubs")
+    // dependsOn("processStubs")
 }
 
-tasks.build {
-    // dependsOn("generateSources")
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group as String
+            artifactId = "hytale-stubs"
+            version = project.version as String
+
+            from(components["java"]) // or "kotlin" for Kotlin projects
+        }
+    }
+
+    repositories {
+        maven {
+            name = "local"
+            url = uri(layout.buildDirectory.dir("maven-repo"))
+        }
+
+        maven {
+            name = "hytaleStubs"
+            url = uri("https://stubs.oglass.dev/repository/")
+            credentials {
+                username = findProperty("mavenUsername") as String? ?: System.getenv("MAVEN_USERNAME")
+                password = findProperty("mavenPassword") as String? ?: System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
 }
